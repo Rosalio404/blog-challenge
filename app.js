3edca6b083cdaeed65b6456eb6990ec6967b7603
Rosalio404 <Rosalio404@protonmail.com>
2020-09-13 23:03:08 -0500

    Add about and contact pages

--- app.js
+++ app.js
@@ -16,11 +16,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//GET
 app.get("/", function(req, res) {
   res.render("home", {hSC: homeStartingContent});
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {aC: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {cC: contactContent});
+});
 
 
 
