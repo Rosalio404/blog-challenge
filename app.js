0175cf3517bbc092f9bc8bbd2ec52dd84316f174
Rosalio404 <Rosalio404@protonmail.com>
2020-09-28 13:35:43 -0500

    Add home post preview rendering

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = 3000;
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
 
 //Mongoose
 mongoose.connect("mongodb://localhost/blogDB", {useNewUrlParser: true});
@@ -27,6 +26,7 @@ const postSchema = new mongoose.Schema({
   content: String,
   date: {type: Date, default: Date.now},
 });
+//Models
 const Post = new mongoose.model("Post", postSchema);
 
 //Express
@@ -37,25 +37,32 @@ app.use(express.static("public"));
 
 //GET
 app.get("/", function(req, res) {
-  res.render("home", {
-    hSC: homeStartingContent,
-    posts: posts
+  Post.find(function (err, posts) {
+    if (!err) {
+      posts.forEach(function (post) {
+        post.title = _.startCase(post.title);
+      });
+      res.render("home", {
+        hSC: homeStartingContent,
+        posts: posts,
+      });
+    } else {
+      console.error(err);
+    }
   });
 });
 
 app.get("/posts/:post", function(req, res) {
   const postURL = _.kebabCase(req.params.post);
-  posts.forEach(function(postObj) {
-    const postObjTitle = _.kebabCase(postObj.title);
-    const postObjContent = postObj.content;
-    if (postObjTitle === postURL) {
+  Post.findOne({title: postURL}, function (err, post) {
+    if (!err) {
       console.log("Match found!");
       res.render("post", {
-        title: postObj.title,
-        content: postObjContent
+        title: _.startCase(post.title),
+        content: post.content
         });
     } else {
-      console.log("No match found.");
+      console.error(err);
     }
   });
 });
@@ -75,7 +82,7 @@ app.get("/compose", function(req, res) {
 //POST
 app.post("/compose", function(req,res) {
   const post = new Post({
-    title: req.body.postTitle,
+    title: _.kebabCase(req.body.postTitle),
     content: req.body.postBody
   });
   post.save(function (err) {
